ab7b13d013ad68a80715d290dc76643ca8c778a5
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2023-03-06 19:52:50 -0700

    Add operate function

--- script.js
+++ script.js
@@ -14,7 +14,22 @@ function divide(num1, num2) {
     return num1 / num2
 }
 
-console.log(add(2, 2))
-console.log(subtract(4, 2))
-console.log(multiply(2, 3))
-console.log(divide(10, 2))
+function operate(num1, num2, operator) {
+    if (operator == "+") {
+        return add(num1, num2)
+    } if (operator == "-") {
+        return subtract(num1, num2)
+    } if (operator == "*") {
+        return multiply(num1, num2)
+    } if (operator == "/") {
+        return divide(num1, num2)
+    } else {
+        return "ERROR"
+    }
+}
+
+console.log(operate(2, 2, "+"))
+console.log(operate(4, 2, "-"))
+console.log(operate(2, 3, "*"))
+console.log(operate(10, 2, "/"))
+console.log(operate(2, 2, 1))
